0723cc498f2be6bce8e2dd8788734b130165126b
Mark <14786851+memersonloh@users.noreply.github.com>
2023-11-09 14:21:13 +0800

    trim

--- main/main.c
+++ main/main.c
@@ -48,7 +48,7 @@ void motor_task(__unused void *params)
     //     moveForward();
     //     gas();
     // }
-    
+
     float startingMag = -1;
 
     float angle = 0;
@@ -62,9 +62,9 @@ void motor_task(__unused void *params)
         lsm303dlh_mag_setup();
         lsm303dlh_read_mag(&mag);
         startingMag = get_angle(&mag);
-        
+
     }
-    
+
 
     while (1)
     {
@@ -72,7 +72,7 @@ void motor_task(__unused void *params)
         lsm303dlh_read_mag(&mag);
         angle = get_angle(&mag);
         diffAngle = angle - startingMag ;
-        
+
         if (diffAngle < -15)
         {
             diffAngle += 360;
@@ -102,7 +102,7 @@ void motor_task(__unused void *params)
     //     lsm303dlh_read_mag(&mag);
     //     angle = get_angle(&mag);
     //     diffAngle = startingMag - angle ;
-        
+
     //     if (diffAngle < -15)
     //     {
     //         diffAngle += 360;
@@ -131,12 +131,12 @@ void motor_task(__unused void *params)
 
 void wheelencoder_task(__unused void *params)
 {
- 
+
     while(1)
     {
-        // int pinState1 = getResults1(); 
+        // int pinState1 = getResults1();
         // getSpeed(pinState1);
-        
+
         // int pinState2 = getResults2();
         // getSpeed2(pinState2);
     }
@@ -164,7 +164,7 @@ void ultrasonic_task(__unused void *params)
 
 void barcode_task(__unused void *params)
 {
-    
+
 
     while(1)
     {
@@ -176,11 +176,11 @@ void barcode_task(__unused void *params)
 
 void magnetometer_task(__unused void *params)
 {
-    
+
    // read
    while (true) {
       // lsm303dlh_acc_setup();
-      
+
       // printf("Acc. X = %5d Y = %5d, Z = %5d \t Mag. X = %4d Y = %4d, Z = %4d \t Angle = %f \r\n",
       //          acc.x,acc.y,acc.z,mag.x,mag.y,mag.z,angle);
     //   printf("Mag. X = %4d Y = %4d, Z = %4d \t Angle = %f \r\n",
@@ -194,7 +194,7 @@ void linereader_task(__unused void *params)
     while(1)
     {
         adc_callback();
-        
+
     }
 }
 
@@ -204,7 +204,7 @@ void vLaunch( void) {
 
     TaskHandle_t motorTask;
     xTaskCreate(motor_task, "MotorThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &motorTask);
-    
+
     // TaskHandle_t wheelencodertask;
     // xTaskCreate(wheelencoder_task, "WheelEncoderThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &wheelencodertask);
 
@@ -213,11 +213,11 @@ void vLaunch( void) {
 
     // TaskHandle_t barcodetask;
     // xTaskCreate(barcode_task, "BarCodeThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY , &barcodetask);
-    
+
     // TaskHandle_t magnetometertask;
     // xTaskCreate(magnetometer_task, "MagnetometerThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &magnetometertask);
 
-    
+
 
 #if NO_SYS && configUSE_CORE_AFFINITY && configNUM_CORES > 1
     // we must bind the main task to one core (well at least while the init is called)
