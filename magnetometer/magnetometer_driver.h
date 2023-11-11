d3898d4b215128905e4f2d17748a2086e0d57205
Mark <14786851+memersonloh@users.noreply.github.com>
2023-11-11 22:43:16 +0800

    sync

--- magnetometer/magnetometer_driver.h
+++ magnetometer/magnetometer_driver.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 #define MAG_SLV 0x1E // Magnetometer slave address.
-#define MAG_OUT 0x03 // Magnetic field data.
+#define MAG_OUT 0x03 // Magnetic field data address.
 #define MAG_CRA 0x00 // CRA_REG_M register: Data output rate bits. These bits set the rate at which data is written to all three data output registers.
 #define MAG_CRB 0x01 // CRB_REG_M register: Gain configuration bits. The gain configuration is common for all channels.
 #define MAG_MRR 0x02 // MR_REG_M register: Mode select bits. These bits select the operation mode of this device.
