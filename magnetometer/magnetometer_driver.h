38d3065b8196105402e2923dfb7420cac88ad532
Mark <14786851+memersonloh@users.noreply.github.com>
2023-11-11 22:41:57 +0800

    sync

--- magnetometer/magnetometer_driver.h
+++ magnetometer/magnetometer_driver.h
@@ -9,9 +9,9 @@
 #define MAG_MRR 0x02 // MR_REG_M register: Mode select bits. These bits select the operation mode of this device.
 
 typedef struct {
-   int16_t x;
-   int16_t y;
-   int16_t z;
+  int16_t x;
+  int16_t y;
+  int16_t z;
 } mag_t;
 
 void lsm303dlh_mag_setup();
