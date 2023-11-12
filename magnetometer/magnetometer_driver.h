c577168c90faced4c48865765f8b7088f1db1135
Mark <14786851+memersonloh@users.noreply.github.com>
2023-11-12 16:17:27 +0800

    Squashed commit of the following:
    commit 2d897d607fd58adc366cdc71dfffa2a909e5183f
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sun Nov 12 16:13:25 2023 +0800
    
        change motor params for testing
    
    commit f72ddde29147158a4140fce96d2025b5e773df14
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sun Nov 12 16:13:14 2023 +0800
    
        remove redundant code
    
    commit d3898d4b215128905e4f2d17748a2086e0d57205
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 22:43:16 2023 +0800
    
        sync
    
    commit 38d3065b8196105402e2923dfb7420cac88ad532
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 22:41:57 2023 +0800
    
        sync
    
    commit dc320b1267858b01cd32ecfb795fbc071eb42c07
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 22:40:44 2023 +0800
    
        tidy up code
    
    commit bb861cf35b22bc75d255f09563ebbdc565db23d5
    Merge: 21592c4 ba044c0
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 21:39:57 2023 +0800
    
        Merge branch 'main' into magnetometer
    
    commit 21592c437e0658d8c171f46faf667977ca0efc0d
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 18:16:35 2023 +0800
    
        updates
    
    commit 2efdc9fd8d29100f7c16a7d99650c825ba426497
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Sat Nov 11 17:42:27 2023 +0800
    
        adjust to 75Hz data rate
    
    commit cfa4ccbc21fcca090821ebd8f04a16e4194a192e
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Thu Nov 9 15:40:12 2023 +0800
    
        sync
    
    commit 518c79cb783cfb72bc68bc62767af8572f337555
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Thu Nov 9 14:44:47 2023 +0800
    
        use correct dependency name
    
    commit 0723cc498f2be6bce8e2dd8788734b130165126b
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Thu Nov 9 14:21:13 2023 +0800
    
        trim
    
    commit cf3ef4cce47f95f8442bc9b63b78d8b40432eedb
    Author: Mark <14786851+memersonloh@users.noreply.github.com>
    Date:   Thu Nov 9 14:19:40 2023 +0800
    
        update driver

--- magnetometer/magnetometer_driver.h
+++ magnetometer/magnetometer_driver.h
@@ -1,38 +1,24 @@
 #ifndef __LSM303DLH__
 #define __LSM303DLH__
+
 #include <stdint.h>
-#define INTERFACE_B 0x1E // Magnetometr slave address
-#define MAG_REG 0x03 // data starts on 0x03 - MSb set to 1 (autoincrease on read) -> 0xA8
-#define MAG_CRA 0x00
-#define MAG_CRB 0x01
-#define MAG_MR 0x02
+#define MAG_SLV 0x1E // Magnetometer slave address.
+#define MAG_OUT 0x03 // Magnetic field data address.
+#define MAG_CRA 0x00 // CRA_REG_M register: Data output rate bits. These bits set the rate at which data is written to all three data output registers.
+#define MAG_CRB 0x01 // CRB_REG_M register: Gain configuration bits. The gain configuration is common for all channels.
+#define MAG_MRR 0x02 // MR_REG_M register: Mode select bits. These bits select the operation mode of this device.
 
 typedef struct {
-   int16_t x;
-   int16_t y;
-   int16_t z;
+  int16_t x;
+  int16_t y;
+  int16_t z;
 } mag_t;
 
-/*
- * Wake up and set the device (see IC documentation for more info)
- * (Documentation: https://cz.mouser.com/datasheet/2/389/stmicroelectronics_cd00260288-1206307.pdf)
- */
 void lsm303dlh_mag_setup();
-
-/*
- * Performs a sequention of i2c communication with INTERFACE_A address
- * and writes in given pointer to mag_t struct
- */
 void lsm303dlh_read_mag(mag_t *mag);
-
-/*
- * Returns a heading angle of the IC
- *
- */
-float get_angle(mag_t *mag);
+float lsm303dlh_get_angle(mag_t *mag);
 
 void magnetometer_init();
 float magnetometer_heading();
 
 #endif
-/* end of lsm303dlh.h */
--- main/main.c
+++ main/main.c
@@ -21,168 +21,134 @@
 #include "pidcontroller.h"
 #include "magnetometer_driver.h"
 
-
 // lab 5 for message buffer
 #include "message_buffer.h"
-
 #define WHEELENCODER_TASK_STACK_SIZE (configMINIMAL_STACK_SIZE * 2)
-
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
-
-
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 
 // message buffer
 #define mbaTASK_MESSAGE_BUFFER_SIZE (60)
 
-#define I2C_BAUD 400 // 400 or 100 (kHz)
-
-
-
 void motor_task(__unused void *params)
 {
-    // while(1)
-    // {
-    //     moveForward();
-    //     gas();
-    // }
-    
-    float startingMag = -1;
+  // while(1)
+  // {
+  //     moveForward();
+  //     gas();
+  // }
 
-    float angle = 0;
-    float diffAngle = 0;
+  float startingMag = -1;
+  float angle = 0;
+  float diffAngle = 0;
 
-    mag_t mag;
+  do {
+    startingMag = magnetometer_heading();
+  } while (startingMag < 0);
 
-    while (startingMag < 0)
+  while (1)
+  {
+    angle = magnetometer_heading();
+    diffAngle = angle - startingMag;
+
+    if (diffAngle < 0) diffAngle += 360;
+
+    printf("staring angle: %f | ending angle: %f | diff angle: %f\n", startingMag, angle, diffAngle);
+
+    if (diffAngle >= 80 && diffAngle <= 100)
     {
-
-        lsm303dlh_mag_setup();
-        lsm303dlh_read_mag(&mag);
-        startingMag = get_angle(&mag);
-        
-    }
-    
-
-    while (1)
-    {
-        lsm303dlh_mag_setup();
-        lsm303dlh_read_mag(&mag);
-        angle = get_angle(&mag);
-        diffAngle = angle - startingMag ;
-        
-        if (diffAngle < -15)
-        {
-            diffAngle += 360;
-        }
-
-        printf("staring angle: %0.6f | ending angle: %0.6f | diff angle: %0.6f\n", startingMag, angle, diffAngle);
-        sleep_ms(25);
-
-
-
-        if (diffAngle >= 80 && diffAngle <= 100)
-        {
-            // Angle has reached or exceeded 90 degrees, stop the car
-            stop();
-            printf("Reached 90 degrees, stopping the car\n");
-            break; // Exit the loop and end the task
-        }
-
-        turnRight();
-        gas();
+      // Angle has reached or exceeded 90 degrees, stop the car
+      stop();
+      printf("Reached 90 degrees, stopping the car\n");
+      break; // Exit the loop and end the task
     }
 
-
-    // while (1)
-    // {
-    //     lsm303dlh_mag_setup();
-    //     lsm303dlh_read_mag(&mag);
-    //     angle = get_angle(&mag);
-    //     diffAngle = startingMag - angle ;
-        
-    //     if (diffAngle < -15)
-    //     {
-    //         diffAngle += 360;
-    //     }
-
-    //     printf("staring angle: %0.6f | ending angle: %0.6f | diff angle: %0.6f\n", startingMag, angle, diffAngle);
-    //     sleep_ms(50);
-
-
-
-    //     if (diffAngle >= 85 && diffAngle <= 95)
-    //     {
-    //         // Angle has reached or exceeded 90 degrees, stop the car
-    //         stop();
-    //         printf("Reached 90 degrees, stopping the car\n");
-    //         break; // Exit the loop and end the task
-    //     }
-
-    //     turnLeft();
-    //     gas();
-    // }
-
-    moveForward();
+    turnRight();
     gas();
+  }
+
+
+  // while (1)
+  // {
+  //     lsm303dlh_mag_setup();
+  //     lsm303dlh_read_mag(&mag);
+  //     angle = get_angle(&mag);
+  //     diffAngle = startingMag - angle ;
+
+  //     if (diffAngle < -15)
+  //     {
+  //         diffAngle += 360;
+  //     }
+
+  //     printf("staring angle: %0.6f | ending angle: %0.6f | diff angle: %0.6f\n", startingMag, angle, diffAngle);
+  //     sleep_ms(50);
+
+
+
+  //     if (diffAngle >= 85 && diffAngle <= 95)
+  //     {
+  //         // Angle has reached or exceeded 90 degrees, stop the car
+  //         stop();
+  //         printf("Reached 90 degrees, stopping the car\n");
+  //         break; // Exit the loop and end the task
+  //     }
+
+  //     turnLeft();
+  //     gas();
+  // }
+
+  // moveForward();
+  // gas();
 }
 
 void wheelencoder_task(__unused void *params)
 {
- 
-    while(1)
-    {
-        // int pinState1 = getResults1(); 
-        // getSpeed(pinState1);
-        
-        // int pinState2 = getResults2();
-        // getSpeed2(pinState2);
-    }
+  while(1)
+  {
+    // int pinState1 = getResults1();
+    // getSpeed(pinState1);
 
+    // int pinState2 = getResults2();
+    // getSpeed2(pinState2);
+  }
 }
 
 void ultrasonic_task(__unused void *params)
 {
-    while(1)
+  while(1)
+  {
+    uint64_t pulseLength = get_measurement_cm();
+    printf("this is distance: %lldcm \n", pulseLength);
+
+    if(pulseLength < 10)
     {
-        uint64_t pulseLength = get_measurement_cm();
-        printf("this is distance: %lldcm \n", pulseLength);
-
-        if(pulseLength < 10)
-        {
-            stop();
-        }
-        else
-        {
-            moveForward();
-        }
-
+      stop();
     }
+    else
+    {
+      moveForward();
+    }
+  }
 }
 
 void barcode_task(__unused void *params)
 {
-    
-
-    while(1)
-    {
-
-        adc_callback();
-        sleep_ms(100);
-    }
+  while(1)
+  {
+    adc_callback();
+    sleep_ms(100);
+  }
 }
 
 void magnetometer_task(__unused void *params)
 {
-    
    // read
    while (true) {
-      // lsm303dlh_acc_setup();
-      
-      // printf("Acc. X = %5d Y = %5d, Z = %5d \t Mag. X = %4d Y = %4d, Z = %4d \t Angle = %f \r\n",
-      //          acc.x,acc.y,acc.z,mag.x,mag.y,mag.z,angle);
+    // lsm303dlh_acc_setup();
+    // printf("Acc. X = %5d Y = %5d, Z = %5d \t Mag. X = %4d Y = %4d, Z = %4d \t Angle = %f \r\n",
+    //          acc.x,acc.y,acc.z,mag.x,mag.y,mag.z,angle);
     //   printf("Mag. X = %4d Y = %4d, Z = %4d \t Angle = %f \r\n",
     //            mag.x,mag.y,mag.z,angle);
     //   sleep_ms(REFRESH_PERIOD);
@@ -191,76 +157,68 @@ void magnetometer_task(__unused void *params)
 
 void linereader_task(__unused void *params)
 {
-    while(1)
-    {
-        adc_callback();
-        
-    }
+  while(1)
+  {
+    adc_callback();
+  }
 }
 
 
 void vLaunch( void) {
+  TaskHandle_t motorTask;
+  xTaskCreate(motor_task, "MotorThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &motorTask);
 
+  // TaskHandle_t wheelencodertask;
+  // xTaskCreate(wheelencoder_task, "WheelEncoderThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &wheelencodertask);
 
-    TaskHandle_t motorTask;
-    xTaskCreate(motor_task, "MotorThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &motorTask);
-    
-    // TaskHandle_t wheelencodertask;
-    // xTaskCreate(wheelencoder_task, "WheelEncoderThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &wheelencodertask);
+  // TaskHandle_t ultrasonictask;
+  // xTaskCreate(ultrasonic_task, "UltraSonicThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY + 1, &ultrasonictask);
 
-    // TaskHandle_t ultrasonictask;
-    // xTaskCreate(ultrasonic_task, "UltraSonicThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY + 1, &ultrasonictask);
+  // TaskHandle_t barcodetask;
+  // xTaskCreate(barcode_task, "BarCodeThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY , &barcodetask);
 
-    // TaskHandle_t barcodetask;
-    // xTaskCreate(barcode_task, "BarCodeThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY , &barcodetask);
-    
-    // TaskHandle_t magnetometertask;
-    // xTaskCreate(magnetometer_task, "MagnetometerThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &magnetometertask);
-
-    
+  // TaskHandle_t magnetometertask;
+  // xTaskCreate(magnetometer_task, "MagnetometerThread", configMINIMAL_STACK_SIZE, NULL, TEST_TASK_PRIORITY, &magnetometertask);
 
 #if NO_SYS && configUSE_CORE_AFFINITY && configNUM_CORES > 1
-    // we must bind the main task to one core (well at least while the init is called)
-    // (note we only do this in NO_SYS mode, because cyw43_arch_freertos
-    // takes care of it otherwise)
-    vTaskCoreAffinitySet(task, 1);
+  // we must bind the main task to one core (well at least while the init is called)
+  // (note we only do this in NO_SYS mode, because cyw43_arch_freertos
+  // takes care of it otherwise)
+  vTaskCoreAffinitySet(task, 1);
 #endif
 
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
+  /* Start the tasks and timer running. */
+  vTaskStartScheduler();
 }
 
 int main( void )
 {
-    init_i2c_default();
-    stdio_init_all();
-    //bus_scanner();
+  stdio_init_all();
+  motor_init();
+  sensor_init();
+  ultrasonic_init();
+  barcode_init();
+  magnetometer_init();
+  sleep_ms(5000);
 
-    motor_init();
-    sensor_init();
-    ultrasonic_init();
-    barcode_init();
-    //magnetometer_init();
-    sleep_ms(5000);
-
-    /* Configure the hardware ready to run the demo. */
-    const char *rtos_name;
+  /* Configure the hardware ready to run the demo. */
+  const char *rtos_name;
 #if ( portSUPPORT_SMP == 1 )
-    rtos_name = "FreeRTOS SMP";
+  rtos_name = "FreeRTOS SMP";
 #else
-    rtos_name = "FreeRTOS";
+  rtos_name = "FreeRTOS";
 #endif
 
 #if ( portSUPPORT_SMP == 1 ) && ( configNUM_CORES == 2 )
-    printf("Starting %s on both cores:\n", rtos_name);
-    vLaunch();
+  printf("Starting %s on both cores:\n", rtos_name);
+  vLaunch();
 #elif ( RUN_FREERTOS_ON_CORE == 1 )
-    printf("Starting %s on core 1:\n", rtos_name);
-    multicore_launch_core1(vLaunch);
-    while (true);
+  printf("Starting %s on core 1:\n", rtos_name);
+  multicore_launch_core1(vLaunch);
+  while (true);
 #else
-    printf("Starting %s on core 0:\n", rtos_name);
-    vLaunch();
+  printf("Starting %s on core 0:\n", rtos_name);
+  vLaunch();
 #endif
-    return 0;
+  return 0;
 }
