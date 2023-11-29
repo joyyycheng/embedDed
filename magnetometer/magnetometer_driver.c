67352dd97e74fdf18bc96aae288a603dc076e077
Mark <14786851+memersonloh@users.noreply.github.com>
2023-11-29 18:03:20 +0800

    add comments for magnetometer

--- magnetometer/magnetometer_driver.c
+++ magnetometer/magnetometer_driver.c
@@ -5,6 +5,7 @@
   GND (e.g. pin 38)  -> GND
 */
 
+// Required Dependencies
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,19 +15,22 @@
 #include "hardware/i2c.h"
 #include "magnetometer_driver.h"
 
-#define I2C_BAUD 100 // 400 or 100 (kHz)
-#define REFRESH_PERIOD 100 // ms
-
+// Parameter Constants
+#define I2C_BAUD 100       // 100 kHz for Baud Rate
+#define REFRESH_PERIOD 100 // 100ms
 #define X_OFFSET -27.5 // Hard-iron Calibration: X offset
 #define Y_OFFSET  20.0 // Hard-iron Calibration: Y offset
 #define MAG_DECL  0.05 //  Magnetic Declination: https://www.magnetic-declination.com/SINGAPORE/SINGAPORE/2443440.html
 
+// Function Prototypes
 void init_i2c_default();
 
-void lsm303dlh_mag_setup() {
+// Setup Function
+void lsm303dlh_mag_setup()
+{
   sleep_ms(1000);
   uint8_t buffer[2];
-  buffer[0] = MAG_CRA; buffer[1] = 0x18; // CRA_REG_M: 75Hz(0x18) 30Hz(0x14) 15Hz(0x10) Data Output Rate
+  buffer[0] = MAG_CRA; buffer[1] = 0x18; // CRA_REG_M: 75Hz(0x18) Data Output Rate
   i2c_write_blocking( i2c_default, MAG_SLV, buffer, 2, true );
   buffer[0] = MAG_CRB; buffer[1] = 0xE0; // CRB_REG_M: Gain X,Y,Z at 230 LSB/Gauss
   i2c_write_blocking( i2c_default, MAG_SLV, buffer, 2, true );
@@ -34,13 +38,16 @@ void lsm303dlh_mag_setup() {
   i2c_write_blocking( i2c_default, MAG_SLV, buffer, 2, false );
 }
 
-void lsm303dlh_read_mag(mag_t *mag) {
+// Read Function (and Hard-iron Calibration for X, Y)
+void lsm303dlh_read_mag(mag_t *mag)
+{
   uint8_t buffer[6];
   int16_t magnet[3];
   uint8_t reg = MAG_OUT;
   i2c_write_blocking( i2c_default, MAG_SLV, &reg, 1, true );
   i2c_read_blocking( i2c_default, MAG_SLV, buffer, 6, false );
   for (int i = 0; i < 3; i++) {
+    // Retrieve Buffer and Insert to magnet[i] based on for-loop i
     magnet[i] = ((buffer[i * 2] << 8 | buffer[(i * 2) + 1] ));
   }
   mag->x = magnet[0] + X_OFFSET; // OUT_X_M: 0x03 & 0x04
@@ -48,7 +55,9 @@ void lsm303dlh_read_mag(mag_t *mag) {
   mag->z = magnet[1];            // OUT_Z_M: 0x05 & 0x06
 }
 
-float lsm303dlh_get_angle(mag_t *mag) {
+// Calculate Angle Function
+float lsm303dlh_get_angle(mag_t *mag)
+{
   // Set for Inverse Z-axis, where cables pointing down
   // Correct for Magnetic Declination
   // Convert Heading from Radian to Degrees
@@ -61,17 +70,22 @@ float lsm303dlh_get_angle(mag_t *mag) {
   return heading;
 }
 
-void magnetometer_init() {
-  init_i2c_default();
-  lsm303dlh_mag_setup();
+// Initialise Sensor Function
+void magnetometer_init()
+{
+  init_i2c_default(); // Call I2C Defaults
+  lsm303dlh_mag_setup(); // Call Magnetometer Setup
 }
 
-float magnetometer_heading() {
-  mag_t mag;
-  lsm303dlh_read_mag(&mag);
-  return lsm303dlh_get_angle(&mag);
+// Output Reading Function
+float magnetometer_heading()
+{
+  mag_t mag; // Initialise Magnetometer Structure
+  lsm303dlh_read_mag(&mag); // Read Magnetometer with Structure
+  return lsm303dlh_get_angle(&mag); // Return Magnetometer Readings as Angles
 }
 
+// Main Function for Testing
 // int main() {
 //   mag_t mag;
 //   stdio_init_all();
@@ -86,10 +100,12 @@ float magnetometer_heading() {
 //   return 0;
 // }
 
-void init_i2c_default() {
-   i2c_init(i2c_default, I2C_BAUD * 1000);
-   gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-   gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-   gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-   gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+// Initialise I2C Defaults
+void init_i2c_default()
+{
+   i2c_init(i2c_default, I2C_BAUD * 1000); // Initialise I2C and set Baud Rate
+   gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C); // Set Serial Data Pin for I2C
+   gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C); // Set Serial Clock Pin for I2C
+   gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN); // Set GPIO Pull Up for SDA Pin
+   gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN); // Set GPIO Pull Up for SCL Pin
 }
